5e781b493b4eab0a9e5f7d2537953e8695efb3c1
Alizadehf-laya <alizadehlayaf@gmail.com>
2024-10-16 09:36:52 +0330

    array added by map read

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,27 @@ import { useState } from 'react'
 
 function App() {
 const [tColorMe,SetColorMe]=useState("blue")
+const [events,Setevents]=useState([
+  {title:"love",id:1},
+  {title:"happy",id:2},
+  {title:"hate",id:3},
+  {title:"anxious",id:4},
+  {title:"thrilled",id:5},
+  {title:"sad",id:6},
+])
 
   return (
     <>
     <div style={{alignItems:"center"}}>
         <h1 style={{color:tColorMe}}>change my color</h1>
         <button onClick={()=>{SetColorMe(tColorMe==="red"?"blue":"red")}}>click to change color</button>
+        {events.map((event,index)=>(
+          <div key={event.id}>
+            <h5>{index+1}-{event.title}</h5>
+          </div>
+        ))}
     </div>
-
+      
     </>
   )
 }
